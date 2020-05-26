93dc39d37ee25bc5f66987d4c85e9d7fcdf7dd12
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-05-26 14:35:24 +0200

    Initial Commit

---
+++ PointcutExpressions_Matching_MethodsAndReturnTypes/src/com/bartek/aopdemo/MainDemoApp.java
@@ -0,0 +1,27 @@
+package com.bartek.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.bartek.aopdemo.dao.AccountDAO;
+import com.bartek.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+		
+		theAccountDAO.addAccount();
+		
+		theMembershipDAO.addSillyMember();
+		
+		context.close();
+	}
+
+}
