10e56fcca8e241b0fccd697e9bcc59d4fbe0d643
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-05-29 13:12:05 +0200

    Initial Commit

---
+++ PointcutExpressions_Combining_Pointcuts/src/com/bartek/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,33 @@
+package com.bartek.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+
+	@Pointcut("execution(* com.bartek.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("execution(* com.bartek.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	@Pointcut("execution(* com.bartek.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()") 
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=====>>> Executing @Before advice on method");
+	}
+	
+	@Before("forDaoPackageNoGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Performing API analytics");
+	}
+}
