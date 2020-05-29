55f03aa20c84297cd3f18569739f0c2fcaa3655c
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-05-30 00:14:04 +0200

    Initial Commit

---
+++ Ordering_Aspects/src/com/bartek/aopdemo/MainDemoApp.java
@@ -0,0 +1,36 @@
+package com.bartek.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.bartek.aopdemo.dao.AccountDAO;
+import com.bartek.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+		
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		theAccountDAO.setName("foobar");
+		theAccountDAO.setServiceCode("silver");
+		
+		String name = theAccountDAO.getName();
+		String code = theAccountDAO.getServiceCode();
+		
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		context.close();
+	}
+
+}
