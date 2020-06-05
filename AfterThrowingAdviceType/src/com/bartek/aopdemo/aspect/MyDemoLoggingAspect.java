9525c6fbd61c7b6d80d0d344fb27e25e005bd2f3
Bartek Walczak <50672367+bl4keee@users.noreply.github.com>
2020-06-05 07:28:36 +0200

    Initial Commit

---
+++ AfterThrowingAdviceType/src/com/bartek/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,75 @@
+package com.bartek.aopdemo.aspect;
+
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import com.bartek.aopdemo.Account;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@AfterThrowing(
+			pointcut="execution(* com.bartek.aopdemo.dao.AccountDAO.findAccounts(..))",
+			throwing="theExc")
+	public void afterThrowingFindAccountsAdvice(
+			JoinPoint theJoinPoint, Throwable theExc) {
+		
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("=====>>> Executing @AfterThrowing on method: " + method);
+		
+		System.out.println("=====>>> The exception is: " + theExc);
+	}
+
+	@AfterReturning(
+			pointcut="execution(* com.bartek.aopdemo.dao.AccountDAO.findAccounts(..))",
+			returning="result")
+	public void afterReturningFindAccountsAdvice(
+					JoinPoint theJoinPoint, List<Account> result) {
+	 
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n=====>>> Executing @AfterReturning on method: " + method);
+		System.out.println("\n=====>>> result is: " + result);
+		
+		convertAccountNamesToUpperCase(result);
+		System.out.println("\n=====>>> result is: " + result);	
+	}
+
+	private void convertAccountNamesToUpperCase(List<Account> result) {
+		for (Account tempAccount : result) {
+			String theUpperName = tempAccount.getName().toUpperCase();
+			tempAccount.setName(theUpperName);
+		}
+	}
+	
+	@Before("com.bartek.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()") 
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+		
+		System.out.println("\n=====>>> Executing @Before advice on method");
+		
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		
+		System.out.println("Method: " + methodSig);
+		
+		Object[] args = theJoinPoint.getArgs();
+		
+		for (Object tempArg: args) {
+			System.out.println(tempArg);
+			
+			if (tempArg instanceof Account) {
+				Account theAccount = (Account) tempArg;
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
+	}
+}
